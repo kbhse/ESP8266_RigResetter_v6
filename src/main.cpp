0bd5ccff9e6b82f4064e76ee7477aad4028f4de9
Karagonat <karagonat@gmail.com>
2019-10-22 07:39:16 +0100

    created workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*  src/main.cpp */
 
 #define PROGNAM "ESP8266_RigResetter"                                                              // program name
-#define VERSION "v6.001"                                                                            // program version (nb lowercase 'version' is keyword)
+#define VERSION "v6.002"                                                                            // program version (nb lowercase 'version' is keyword)
 #define PGMFUNCT "Remotely power-cycle a crypto mining rig"                                        // what the program does
 #define HARDWARE "Rig Resetter, ESP8266 Breakout, v6.0"                                            // hardware version
 #define AUTHOR "J Manson"                                                                          // created by
