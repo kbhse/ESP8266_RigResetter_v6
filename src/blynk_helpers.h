d64733c20e7d9d8753a878ed900a8b8d9ea1325b
Karagonat <karagonat@gmail.com>
2019-10-25 06:45:23 +0100

    created blynk_helpers.h

---
+++ src/blynk_helpers.h
@@ -0,0 +1,38 @@
+/* blynk_helpers.h */
+
+#ifndef blynk_helpers_H   /* include guards */
+#define blynk_helpers_H
+
+// blynk_routines helper functions
+// -------------------------------
+
+// set the pins of the PCF8574 MP device
+// and the PCF8574 P device
+// why is this in blynk_routines ???
+void setPortPins();
+
+// read the state of the pcb inputs
+// called by timer (timer.setInterval(500L, getPcbInputs)) in setup()
+// calls: getPowerLedAState(), getPowerLedBState(), getAuxInputState()
+void getPcbInputs();
+
+// reads the SHT30 sensor
+// called by a timer (timer.setInterval(60000L, readSHT30Sensor)) in the setup() function
+// data will be sent to V42 (temperature) and V43 (humidity) widgets in the app
+void readSHT30Sensor();
+
+// the (global) heartbeatFlags are set by the SMOS_SRR_UPD routine when valid keep-alive messages are received (approx every 2 seconds).
+// this function will be called by a timer (timer.setInterval(5000L, flashHeartbeats)) in the setup() function.
+// it sends a message to the LED widget to turn ON and starts a timer to turn it OFF again.
+// it then resets the heartbeatFlag.
+// uses 2 instances of the WidgetLED object, WidgetLED heartbeatLedA(V3) and WidgetLED heartbeatLedB(V7) and their setValue() methods.
+void flashHeartbeats();
+
+// log startup info to terminal
+void logStartup();
+
+// log the states of the Motherboard Power LEDs on startup
+void logMBPowerLedStates();
+
+
+#endif // blynk_helpers_H
